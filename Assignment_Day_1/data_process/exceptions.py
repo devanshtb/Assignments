8f34b687a230303df2d7805970eb373d0a360180
devanshtb <devanshupadhyay@vebuin.com>
2026-01-06 17:14:07 +0530

    Delete Assignment_Day_1 directory

--- Assignment_Day_1/data_process/exceptions.py
+++
@@ -1,7 +0,0 @@
-class InvalidNumberError(ValueError):  # Raised when a value cannot be converted to a number
-    pass
-
-
-class NoValidDataError(ValueError):
- pass
-                                    #Raised when no valid numeric data remains after filtering 
